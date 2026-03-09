c64af24f39e65627753f20a1f04b98c91d73c1de
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-09 21:19:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# -----------------------------
+# DATABASE SETUP
+# -----------------------------
+
+engine = create_engine("sqlite:///story_game.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
 
